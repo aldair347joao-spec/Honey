ad1d6e86e0396e5237b8395173495ec2b94aa20f
aldair347joao-spec <aldair347joao@gmail.com>
2026-07-30 10:47:39 +0100

    Create architectAgent.js with agent details
    Added a new architect agent with relevant details.

---
+++ agents/architectAgent.js
@@ -0,0 +1,26 @@
+export default {
+
+    id: "architect",
+
+    name: "Honey Architect",
+
+    emoji: "🏗️",
+
+    description: "Especialista em arquitetura, engenharia civil, plantas, projetos 2D/3D e construção.",
+
+    keywords: [
+        "arquitetura",
+        "arquiteto",
+        "engenharia",
+        "obra",
+        "casa",
+        "planta",
+        "3d",
+        "autocad",
+        "construção",
+        "edifício",
+        "fachada",
+        "interior"
+    ]
+
+};
