efacb6c88cfbcad0ce99f8f69df6785a6e372f57
aldair347joao-spec <aldair347joao@gmail.com>
2026-07-30 10:45:27 +0100

    Create educationAgent.js with agent details
    Added education agent with relevant details and keywords.

---
+++ agents/educationAgent.js
@@ -0,0 +1,27 @@
+export default {
+
+    id: "education",
+
+    name: "Honey Education",
+
+    emoji: "🎓",
+
+    description: "Especialista em educação, escolas, universidades, professores, aulas, exercícios e materiais didáticos.",
+
+    keywords: [
+        "escola",
+        "universidade",
+        "professor",
+        "aluno",
+        "educação",
+        "ensino",
+        "matemática",
+        "física",
+        "química",
+        "biologia",
+        "história",
+        "prova",
+        "curso"
+    ]
+
+};
